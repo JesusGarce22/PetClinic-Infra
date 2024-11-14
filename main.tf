5f80dd8698fb60b4b4fcf17f02b8b5273e0bac95
Jesus Garces <82425534+JesusGarce22@users.noreply.github.com>
2024-11-14 23:39:10 +0000

    change storage acconut name

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "azurerm_role_assignment" "aks_acr_role_assignment" {
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "divergenciaStorageAccount"
+  name                     = "divergenciaSA"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
