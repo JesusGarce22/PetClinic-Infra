e3edfcdcd1bc6a70dea4d3dc577ab3df91ed72b4
Jesus Garces <82425534+JesusGarce22@users.noreply.github.com>
2024-11-15 00:05:40 +0000

    put rg name in terraform block

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #tf remote backend
 terraform {
   backend "azurerm" {
-    resource_group_name   = var.resource_group_name
+    resource_group_name   = "az_rg"
     storage_account_name  = "divergenciaSA"
     container_name        = "terraform-state"
     key                   = "terraform.tfstate"
